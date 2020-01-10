ed6b7da467f71a81a014e6b94984dad5893aa0b5
theochupp <tclchiam@gmail.com>
2020-01-10 17:29:58 -0500

    feat: add an endpoint that supports a slash command for slack

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,39 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as express from "express";
+import * as cors from "cors";
+import {SectionBlock} from "@slack/types";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+const markdownSection = (text: string): SectionBlock => {
+    return ({
+        type: "section",
+        text: {
+            type: "mrkdwn",
+            text: text
+        }
+    });
+};
+
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// Command endpoints
+//
+
+const commandsApp: express.Application = express();
+commandsApp.use(cors({origin: true}));
+commandsApp.post("/action", (request: express.Request, response: express.Response) => {
+    console.log(request.body);
+
+    const fullCommandText = request.body.text.toString();
+    const commandType = fullCommandText.split(" ");
+    // const responseUrl = request.body.response_url.toString();
+
+    const responseBody = {
+        response_type: "in_channel",
+        blocks: [
+            markdownSection(`Okay! I've recorded your action item. commandType=${commandType}`),
+        ]
+    };
+
+    response.status(200).send({...responseBody});
 });
+
+export const commands = functions.https.onRequest(commandsApp);
