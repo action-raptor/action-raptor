bac1be2ef1c48cdb344a65375804021d81d523a8
theochupp <theo@autonomic.ai>
2020-02-21 15:39:29 -0500

    refactor: try using Reader monads to compose effect-ful actions

--- src/handlers/auth.handler.ts
+++ src/handlers/auth.handler.ts
@@ -1,29 +1,60 @@
 import * as express from "express";
 import * as rp from "request-promise";
 import {Client} from "pg";
+
+import {ReaderTaskEither} from "fp-ts/lib/ReaderTaskEither";
+import {tryCatch} from "fp-ts/lib/TaskEither";
+
 import {clientId, clientSecret} from "../config";
 
-export const oauthRedirectHandler = (client: Client) => {
-    return (request: express.Request, response: express.Response) => {
+export type OAuthRedirectDependencies = {
+    client: Client
+    rpApi: rp.RequestPromiseAPI
+}
+
+export type OAuthRedirectError =
+    { _type: "RetrieveAuthTokenFromSlackError", message: string }
+    | { _type: "UpsertAuthTokenIntoPsqlError", message: string }
+    ;
+
+export const oauthRedirectHandler = (dependencies: OAuthRedirectDependencies) =>
+    (request: express.Request, response: express.Response) => {
         console.log(`handling oauth redirect`);
 
-        rp.post(`https://slack.com/api/oauth.v2.access`, {
+        retrieveAuthTokenFromSlack(request.query.code)
+            .chain((resp) => upsertAuthTokenIntoPsql(resp.access_token, resp.team.id))
+            .run(dependencies)
+            .then(() => {
+                console.log(`saved access token`);
+                response.send("success!");
+            })
+            .catch((err) => {
+                console.log(`error saving access token: ${err}`);
+                response.send("something went wrong");
+            });
+    };
+
+const retrieveAuthTokenFromSlack = (code: string) => new ReaderTaskEither<OAuthRedirectDependencies, OAuthRedirectError, any>(({rpApi}) =>
+    tryCatch(
+        // @ts-ignore
+        () => rpApi.post(`https://slack.com/api/oauth.v2.access`, {
             form: {
-                code: request.query.code,
+                code: code,
                 client_id: clientId,
                 client_secret: clientSecret
             },
             json: true
-        }).then((resp) => {
-            const query = `INSERT INTO token(value, workspace) VALUES ($1, $2)`;
-            const values = [resp.access_token, resp.team.id];
-            return client.query(query, values);
-        }).then(() => {
-            console.log(`saved access token`);
-            response.send("success!");
-        }).catch((err) => {
-            console.log(`error saving access token: ${err}`);
-            response.send("something went wrong");
-        });
-    }
-};
+        }),
+        (err: any) => ({_type: "RetrieveAuthTokenFromSlackError", message: err.toString()})
+    ));
+
+const upsertAuthTokenIntoPsql = (accessToken: string, teamId: string) => new ReaderTaskEither<OAuthRedirectDependencies, OAuthRedirectError, void>(({client}) => {
+    const query = `INSERT INTO token(value, workspace) VALUES ($1, $2)`;
+    const values = [accessToken, teamId];
+
+    return tryCatch<OAuthRedirectError, any>(
+        () => client.query(query, values),
+        (err: any) => ({_type: "UpsertAuthTokenIntoPsqlError", message: err.toString()})
+    )
+        .map(() => ({}) as unknown as void);
+});
