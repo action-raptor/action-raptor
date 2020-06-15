61ffebda96ae6a4a87abf20daf40d9a4d42e3f6b
chance <chance.a.cyphers@gmail.com>
2020-06-15 18:28:25 -0400

    post in channel based on crons stored in db

---
+++ migrations/1592258231605_reminders.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+const autoUpdatingTimestamp = (pgm: MigrationBuilder) => ({
+    type: "timestamp",
+    notNull: true,
+    default: pgm.func("current_timestamp")
+});
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+    pgm.createTable("reminder", {
+        id: "id",
+        created_at: autoUpdatingTimestamp(pgm),
+        last_updated: autoUpdatingTimestamp(pgm),
+        workspace_id: {type: "varchar(1000)", notNull: true},
+        channel_id: {type: "varchar(1000)", notNull: true},
+        cron: {type: "varchar(100)", notNull: true}
+    });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+    pgm.dropTable("reminder");
+}
