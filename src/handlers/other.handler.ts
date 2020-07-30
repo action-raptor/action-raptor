40b4d71a32f60415492ca854c0d63a8ec06ccd6e
chance <chance.a.cyphers@gmail.com>
2020-07-29 20:20:07 -0400

    add privacy policy link to landing

--- src/handlers/other.handler.ts
+++ src/handlers/other.handler.ts
@@ -15,7 +15,11 @@ export const supportHandler = (request: express.Request, response: express.Respo
 const landing = `
 <h1>Action Raptor</h1>
 <p>rawr</p>
+<p>This app helps you to keep your team and yourself accountable for action items people have committed to. You can add and complete items. You can have your team be reminded of the items. Behind the scenes, we'll keep track of some analytics and show them to each user on the app's home page.
+</p>
+<p>Click the link below to install!!</p>
 <a href="https://slack.com/oauth/v2/authorize?client_id=6124229605.904278581350&scope=chat:write,commands&user_scope="><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+<p>See our privacy policy <a href="https://action-raptor.herokuapp.com/privacy-policy">here</a></p>
 `;
 
 const supportal = `
