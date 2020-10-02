19e32d7d1187a45c752d53482bf56b3cc2384aeb
theochupp <tclchiam@gmail.com>
2020-10-02 15:54:16 -0400

    refactor: reminders now uses the slack.WebClient

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {App, ExpressReceiver} from "@slack/bolt";
+import {App, BlockAction, ExpressReceiver, ViewSubmitAction} from "@slack/bolt";
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import {RequestPromiseAPI} from "request-promise";
@@ -8,9 +8,14 @@ import {landingHandler, privacyHandler, supportHandler} from "./handlers/other.h
 
 import {actionCommandHandler} from "./handlers/slash_action.handler";
 import {appHomeOpenedHandler} from "./handlers/event.app_home_opened.handler";
-import {blockActionHandler} from "./handlers/block_action.handler";
 import {oauthRedirectHandler} from "./handlers/auth.handler";
 import {setupReminders} from "./reminders";
+import {addActionItemActionHandler} from "./handlers/action.add_action_item.handler";
+import {postToChannelActionHandler} from "./handlers/action.post_to_channel.handler";
+import {closeMenuActionHandler} from "./handlers/action.close_menu.handler";
+import {completeActionItemActionHandler} from "./handlers/action.complete_action_item.handler";
+import {arMenuActions} from "./model.menu_actions";
+import {openActionItemModalActionHandler} from "./handlers/action.open_action_item_modal.handler";
 
 export type AppDependencies = {
     pool: Pool
@@ -21,7 +26,7 @@ export async function buildApp(dependencies: AppDependencies) {
     const receiver = new ExpressReceiver({signingSecret: process.env.SLACK_SIGNING_SECRET!});
     const app = new App({
         token: process.env.SLACK_TOKEN,
-        receiver
+        receiver,
     });
 
     receiver.router.use(cors({origin: true}));
@@ -29,7 +34,16 @@ export async function buildApp(dependencies: AppDependencies) {
     receiver.router.use(bodyParser.urlencoded({extended: true}));
 
     app.command("/action", actionCommandHandler.run(dependencies));
-    receiver.router.post("/action/block", blockActionHandler(dependencies.pool));
+
+    app.action<BlockAction>({action_id: arMenuActions.openActionItemModal}, openActionItemModalActionHandler.run(dependencies));
+    app.action<BlockAction>({action_id: arMenuActions.postToChannel}, postToChannelActionHandler.run(dependencies));
+    app.action<BlockAction>({action_id: arMenuActions.closeMenu}, closeMenuActionHandler.run(dependencies));
+    app.action<BlockAction>({action_id: new RegExp(`^${arMenuActions.completeActionItem}`)}, completeActionItemActionHandler.run(dependencies));
+
+    app.view<ViewSubmitAction>(
+        {type: "view_submission", callback_id: arMenuActions.addActionItem},
+        addActionItemActionHandler.run(dependencies)
+    );
 
     app.event<"app_home_opened">("app_home_opened", appHomeOpenedHandler.run(dependencies));
 
@@ -39,7 +53,10 @@ export async function buildApp(dependencies: AppDependencies) {
     receiver.router.get("/privacy-policy", privacyHandler);
     receiver.router.get("/support", supportHandler);
 
-    await setupReminders(dependencies.pool);
+    await setupReminders({
+        ...dependencies,
+        client: app.client,
+    });
 
     return app;
 }
